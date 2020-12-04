2064b4978d1da1fae0067ba194e4c540d97b526e
Nikita <nicitazaletin@yandex.ru>
2020-12-05 01:22:31 +0300

    645

---
+++ index.js
@@ -0,0 +1,47 @@
+let texts=
+[
+"Лицо со шрамом",
+"Бригада", 
+"Звёздыный войны",
+"Гарри Поттер",
+"Бэтмен",
+"Шерлок Холмс",
+"Мстители: Война бесконечности",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+]
+let names=
+[
+"Тони Монтана",
+"Саша 'Белый'", 
+"Дарт Вейдер",
+"Волан-де-Морт",
+"Джокер",
+"Джим Мориарти",
+"Танос",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+"Текст",
+];
+let films = document.querySelectorAll('.film');
+
+
+for(let i=0; i < films.length; i++){
+    films[i].style.backgroundImage = "url('images/"+i+".jpg')";
+    films[i].childNodes[1].textContent = texts[i];
+    films[i].childNodes[3].textContent = names[i];
+    films[i].addEventListener('click', function(e){     
+        let id = e.target.id; 
+        localStorage.setItem("id",id);
+        document.location.href='good_1.html';
+      });
+}
--- js.js
+++
@@ -1,7 +0,0 @@
-let texts=["1983/Лицо со шрамом FFFFFFFFFFFFFFF"]
-let images=["url('1.jpg')"]
-let films = document.querySelectorAll('.film');
-
-for(let i=0; i < films.length; i++){
-    films[i].style.backgroundImage = images[0];
-}
