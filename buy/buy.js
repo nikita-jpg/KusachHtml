87fa7f1b203ca3247166dc060aadf934d6162003
Nikita <nicitazaletin@yandex.ru>
2020-12-07 10:20:54 +0300

    test

--- buy/buy.js
+++ buy/buy.js
@@ -1,31 +1,99 @@
 let buyWindow;
 let buyBack;
+let width;
+let adresses = ["проспект Вернадского, дом 86, стр.1","Мичуринский проспект, д. 12, корпус 2","ул. Студенческая, дом 33, корп. 3."];
 
 
 
 function fillBuyWIndow(){
-    buyWindow.querySelector(".dialog_title").querySelector("p").textContent="Покупка";
-    buyWindow.querySelector(".dialog_title").querySelector("img").addEventListener('click', function(e){     
-        buy_close();
-      });
-
-}
-function make(){
-    let purchase = document.createElement('div');
+    buyWindow = document.createElement('div');
+    buyWindow.classList.add("buyWindow");
+    buyWindow.appendChild(makePayment());
+    buyWindow.appendChild(makeAddress());
+    buyWindow.appendChild(makePersInf());
 }
 
+function makePayment(){
+    //Контейнер для оплаты
+    let payment = document.createElement('div');
+    payment.classList.add("payment");
+    payment.style.width = width;
+
+    //Поле "Способ оплаты"
+    let paymentText = document.createElement('p');
+    paymentText.classList.add("payment_text");
+    paymentText.textContent = "Оплата";
+    payment.append(paymentText);
+
+    //Поле Оплата картой
+    let payment_card = document.createElement('input');
+    payment_card.classList.add("payment_card");
+    payment_card.type = "text";
+    payment_card.placeholder="Введите номер карты";
+    payment.append(payment_card);
+
+    //Кнопка подтвердить
+    let payment_card_enter = document.createElement("div");
+    payment_card_enter.classList.add("payment_card_enter");
+    payment_card_enter.textContent = "Подтвердить";
+    payment.appendChild(payment_card_enter);
+
+
+    return payment;
+}
+function makeAddress(){
+
+    //Контейнер для адресов
+    let adress = document.createElement('div');
+    adress.classList.add("adress");
+    adress.style.width = width;
+    adress.style.height = "200px";
+    adress.style.maxHeight = "170px";
+    adress.textContent = "Адреса для самовывоза";
+
+    //Маркированный список адресов
+    let list = document.createElement('ul');
+    for (let i=0;i<3;i++){
+        let elem = document.createElement('li');
+        elem.style.height = "35px";
+        elem.textContent = adresses[i];
+        list.appendChild(elem);
+    }
+    adress.appendChild(list);
+
+    return adress;
+}
+
+function makePersInf(){
+    let phone = makePayment();
+    phone.querySelector(".payment_text").textContent = "Номер телефона";
+    phone.querySelector(".payment_card").placeholder = "Введите номер телефона";
+    return phone;
+}
+
+function fillByWidth(){
+    if(screen.width<768){
+        width = "310px";
+    }else if (screen.width<1280){
+        width = "350px";
+    }else{
+        width = "400px";
+    }
+}
 
 function buy(){
+    fillByWidth();
 
-    buyWindow = getWindow();
+    sentWindow = getWindow();
+    sentWindow.querySelector(".dialog_title").querySelector("p").textContent="Покупка";
+    sentWindow.querySelector(".dialog_title").querySelector("img").addEventListener('click', function(e){     
+        buy_close();});
+
     fillBuyWIndow();
-    let a = document.createElement('div');
-    a.style.height = "40px";
-    buyWindow.appendChild(a);
+    sentWindow.appendChild(buyWindow)
     
     buyBack = getBlurBack();
-    buyBack.appendChild(buyWindow);
-
+    buyBack.appendChild(sentWindow);
 
     document.body.append(buyBack);
 }
--- index.js
+++ index.js
@@ -1,6 +1,43 @@
+let filmsInRow = 7;
+let rows = 2;
+let qurId = 0;
 
+
+
+//Создаём строки
+for( let i=0;i<rows;i++){
+  document.body.appendChild(getFilmRow());
+}
+
+function getFilmRow()
+{
+  let row = document.createElement('div');
+  row.classList.add('filmRow');
+  for(let i=0;i<filmsInRow;i++)
+    row.appendChild(getFilm());
+  
+    return row;
+}
+
+function getFilm(){
+  let film = document.createElement('div');
+  film.classList.add('film');
+  film.id = qurId;
+
+  let infoOne = document.createElement('div');
+  infoOne.classList.add('info');
+  let infoTwo = document.createElement('div');
+  infoTwo.classList.add('info');
+
+  film.appendChild(infoOne);
+  film.appendChild(infoTwo);
+
+  qurId++;
+  return film;
+}
+
+//Заполняем фильмы
 let films = document.querySelectorAll('.film');
-
 for(let i=0; i < films.length; i++){
     films[i].style.backgroundImage = "url('images/"+i+".jpg')";
     films[i].childNodes[1].textContent = heroes[i].filmName;
