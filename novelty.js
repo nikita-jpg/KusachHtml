42504868e6e277dd3ed4ecad684b79a22cbbece1
Nikita <nicitazaletin@yandex.ru>
2020-12-26 02:04:22 +0300

    окно с трейлерами

--- novelty.js
+++ novelty.js
@@ -10,11 +10,37 @@ function fillNoveltyWindow(){
     let films = document.querySelectorAll('.film');
     for(let i=0; i < films.length; i++){
         films[i].style.backgroundImage = "url('images/novelty/"+i+".jfif')";
-        films[i].childNodes[1].textContent = noveltyList[i];
+        films[i].childNodes[1].textContent = noveltyList[i].name;
         films[i].addEventListener('click', function(e){     
-            let id = e.currentTarget.id; 
-            localStorage.setItem("id",id);
-            document.location.href='hero/hero.html';
+            trailer_open();
         });
     }
+}
+
+function trailer_open()
+{
+    document.body.style.overflow = "hidden";
+    myBackground = getBlurBack();
+    myWindow = getWindow();
+    myWindow.querySelector(".dialog_title").querySelector("p").textContent="Трейлер";
+    myWindow.querySelector('img').src = "svg/close.svg";
+    myWindow.querySelector(".dialog_title").querySelector("img").addEventListener('click', function(e){     
+        trailer_close();
+    });
+
+    let noveltyContainer = document.createElement('iframe');
+    noveltyContainer.id = "noveltyContainer";
+    noveltyContainer.src = "https://www.youtube.com/embed/3jT_q7dt-cM";
+    
+
+    myWindow.appendChild(noveltyContainer);
+    myBackground.appendChild(myWindow);
+    document.body.append(myBackground);
+}
+
+function trailer_close()
+{
+    qurSouvNum = 0;
+    document.body.style.overflow = "auto";
+    document.body.removeChild(myBackground);
 }
