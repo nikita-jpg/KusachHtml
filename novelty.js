55b0f64f4a7014e37249877bad77feff1ee1a3b1
Nikita <nicitazaletin@yandex.ru>
2020-12-26 00:59:24 +0300

    частично готово

--- novelty.js
+++ novelty.js
@@ -1,6 +1,20 @@
+let noveltyPage = document.querySelector('#noveltyPage');
 fillNoveltyWindow();
 
 
 function fillNoveltyWindow(){
-    document.body.appendChild(getMainHeader());
+    noveltyPage.appendChild(getMainHeader());
+    noveltyPage.appendChild(getFilmBlock());
+
+    //Заполняем фильмы
+    let films = document.querySelectorAll('.film');
+    for(let i=0; i < films.length; i++){
+        films[i].style.backgroundImage = "url('images/novelty/"+i+".jfif')";
+        films[i].childNodes[1].textContent = noveltyList[i];
+        films[i].addEventListener('click', function(e){     
+            let id = e.currentTarget.id; 
+            localStorage.setItem("id",id);
+            document.location.href='hero/hero.html';
+        });
+    }
 }
